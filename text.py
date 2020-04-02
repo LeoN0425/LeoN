53f1638e615ad347d01da033fce8bad7924cfa2b
Leo <1139378133@qq.com>
2020-04-02 19:06:17 +0800

    5th

--- text.py
+++ text.py
@@ -15,3 +15,14 @@ for line in readline:
         if i!='':
             words.append(i.lower())#去除掉所有零元素，并转化为小写
 stat=[]
+numstat=[0]*len(words)
+for i in words:
+    if i not in stat:
+        stat.append(i)
+        s=stat.index(i)
+        numstat[s]+=1
+    else:
+        s=stat.index(i)
+        numstat[s]+=1#将每个单词不重复地放入列表中，并统计出现次数
+nummax=[]
+wordmax=[]
