f5fe498dc3445884055b54ba5e233a9463e2a4c7
Leo <1139378133@qq.com>
2020-04-02 19:05:36 +0800

    4th

--- text.py
+++ text.py
@@ -5,3 +5,13 @@ art=open(r'C:\English Test.txt')#读取文件
 punctuation='!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~…0123456789'#标点符号 
 readline=art.readlines()
 words=[]
+for line in readline:
+    line=line.strip()#去除首尾空格
+    for i in line:
+        if i in punctuation:
+            line=line.replace(i,' ')#讲标点符号替换为空格
+    word=line.split(' ')#以空格为界划分文本
+    for i in word:
+        if i!='':
+            words.append(i.lower())#去除掉所有零元素，并转化为小写
+stat=[]
