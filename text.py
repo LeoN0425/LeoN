0047aa61fb6d5457d7ecbe06872eb3b7a7db3476
Leo <1139378133@qq.com>
2020-04-02 19:04:51 +0800

    3th

--- text.py
+++ text.py
@@ -1,2 +1,7 @@
 import matplotlib.pyplot as plt
 import tkinter as tk
+
+art=open(r'C:\English Test.txt')#读取文件
+punctuation='!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~…0123456789'#标点符号 
+readline=art.readlines()
+words=[]
